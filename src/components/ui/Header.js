0531b729d1f117a432111be4ebfb0b5662134d8e
James Mccarthy <mccarthyjames35@yahoo.com>
2024-08-25 13:02:18 -0700

    working on header

---
+++ src/components/ui/Header.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { AppBar, useScrollTrigger } from '@material-ui/core';
+import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+
+import logo from '../../assets/logo.svg';
+
+function ElevationScroll(props) {
+	const { children } = props;
+
+	const trigger = useScrollTrigger({
+		disableHysteresis: true,
+		threshold: 0
+	});
+
+	return React.cloneElement(children, {
+		elevation: trigger ? 4 : 0
+	});
+}
+
+const useStyles = makeStyles((theme) => ({
+	toolbarMargin: {
+		...theme.mixins.toolbar,
+		marginBottom: '3em'
+	},
+	logo: {
+		height: '7em'
+	}
+}));
+export default function Header(props) {
+	const classes = useStyles();
+	return (
+		<>
+			<ElevationScroll>
+				<AppBar position='fixed'>
+					<Toolbar disableGutters>
+						<img
+							src={logo}
+							alt='logo'
+							className={classes.logo}
+						/>
+					</Toolbar>
+				</AppBar>
+			</ElevationScroll>
+			<div className={classes.toolbarMargin} />
+		</>
+	);
+}
