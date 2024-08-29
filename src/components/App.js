4591b85cf52442a55ed859d89e364944cd4e6a0e
James Mccarthy <mccarthyjames35@yahoo.com>
2024-08-29 14:32:47 -0700

    loading new project for now

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from '../components/ui/Header';
 import theme from './ui/Theme';
 import Footer from './ui/Footer';
+import LandingPage from './LandingPage';
 
 function App() {
 	const [selectedIndex, setSelectedIndex] = useState(0);
@@ -19,7 +20,7 @@ function App() {
 				/>
 				<div style={{ marginTop: '4.5em' }}></div>
 				<Routes>
-					<Route path='/' element={<div>Home</div>} />
+					<Route path='/' element={<LandingPage />} />
 					<Route path='/services' element={<div>Services</div>} />
 					<Route path='/customsoftware' element={<div>Custom Software</div>} />
 					<Route path='/mobileapps' element={<div>Mobile Apps</div>} />
